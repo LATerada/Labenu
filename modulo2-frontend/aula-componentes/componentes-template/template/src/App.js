3881d923f7aadebc9d702c9dcb56dea6510a122a
LATerada <larissaakiterada@gmail.com>
2023-01-16 18:36:21 -0300

    Adicionando projeto-intro-web completo e finalizado

---
+++ modulo2-frontend/aula-componentes/componentes-template/template/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import logo from "./img/logo.png";
+
+export default function App() {
+  
+
+  return (
+    <div className="App">
+      
+     <h1>Aula - Componentes React</h1>
+    </div>
+  );
+}
