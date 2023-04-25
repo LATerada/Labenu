e2102e78b8da649bd7e29961ba4358646e877873
LATerada <larissaakiterada@gmail.com>
2023-04-25 12:11:31 -0300

    :construction: Add Layered Architecture

---
+++ modulo3-backend/aula-arquitetura-II/arquitetura-II-exercicios/src/database/CourseDatabase.ts
@@ -0,0 +1,39 @@
+import { CourseDB } from "../types";
+import { BaseDataBase } from "./BaseDatabase";
+
+export class CourseDatabase extends BaseDataBase {
+  public static TABLE_COURSES = "courses";
+
+  public async findCourses(q: string | undefined): Promise<CourseDB[]> {
+    if (q) {
+      const courseDB: CourseDB[] = await BaseDataBase.connection(
+        CourseDatabase.TABLE_COURSES
+      ).where("name", "LIKE", `%${q}%`);
+      return courseDB;
+    } else {
+      const courseDB: CourseDB[] = await BaseDataBase.connection(
+        CourseDatabase.TABLE_COURSES
+      );
+      return courseDB;
+    }
+  }
+
+  public async findCourseById(id: string): Promise<CourseDB | undefined> {
+    const [courseDB]: CourseDB[] | undefined[] = await BaseDataBase.connection(
+      CourseDatabase.TABLE_COURSES
+    ).where({ id });
+    return courseDB;
+  }
+
+  public async insertCourse(newCourse: CourseDB): Promise<void> {
+    await BaseDataBase.connection(CourseDatabase.TABLE_COURSES).insert(
+      newCourse
+    );
+  }
+
+  public async removeCourse(id: string): Promise<void> {
+    await BaseDataBase.connection(CourseDatabase.TABLE_COURSES)
+      .del()
+      .where({ id });
+  }
+}
