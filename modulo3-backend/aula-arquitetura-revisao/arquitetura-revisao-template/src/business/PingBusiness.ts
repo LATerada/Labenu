a851712a0e985e28964a793850104e0581e362a8
LATerada <larissaakiterada@gmail.com>
2023-04-27 17:24:48 -0300

    Update aula-arquitetura-revisao

--- modulo3-backend/aula-arquitetura-revisao/arquitetura-revisao-template/src/business/PingBusiness.ts
+++ modulo3-backend/aula-arquitetura-revisao/arquitetura-revisao-template/src/business/PingBusiness.ts
@@ -1,6 +1,7 @@
 export class PingBusiness {
   getPing = async (): Promise<string> => {
     const output = "Pong!";
+    console.log(output);
     return output;
   };
 }
--- modulo3-backend/aula-arquitetura-revisao/arquitetura-revisao-template/src/index.ts
+++ modulo3-backend/aula-arquitetura-revisao/arquitetura-revisao-template/src/index.ts
@@ -1,18 +1,18 @@
-import express from "express";
-import cors from "cors";
-import { productRouter } from "./routers/productRouter";
-import { brandRouter } from "./routers/brandRouter";
-import { pingRouter } from "./routers/pingRouter";
+import express from 'express'
+import cors from 'cors'
+import { pingRouter } from './routers/pingRouter'
+import { brandRouter } from './routers/brandRouter'
+import { productRouter } from './routers/productRouter'
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json);
+app.use(cors())
+app.use(express.json())
 
-app.listen(3003, () => console.log(`Servidor rodando na porta ${3003}`));
-
-app.use("/ping", pingRouter);
-
+app.listen(3003, () => {
+    console.log(`Server running on port: ${3003}`)
+})
+app.use("/ping", pingRouter)
 app.use("/products", productRouter);
 
 app.use("/brands", brandRouter);
