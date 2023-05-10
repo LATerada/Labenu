ddb0bcab0c3c9c2bb45a19ff3d11c5c0a82fb077
LATerada <larissaakiterada@gmail.com>
2023-05-10 12:23:06 -0300

    Resolucao dos exercicios da aula-testes-backend-III

---
+++ modulo4-fullstack/aula-testes-backend-III/testes-backend-III-exercicios/tests/business/product/createProduct.test.ts
@@ -0,0 +1,119 @@
+import { ZodError } from "zod";
+import { ProductBusiness } from "../../../src/business/ProductBusiness";
+import { CreateProductSchema } from "../../../src/dtos/product/createProduct.dto";
+import { BadRequestError } from "../../../src/errors/BadRequestError";
+import { IdGeneratorMock } from "../../mocks/IdGeneratorMock";
+import { ProductDatabaseMock } from "../../mocks/ProductDatabaseMock";
+import { TokenManagerMock } from "../../mocks/TokenManagerMock";
+
+describe("Testando createProduct", () => {
+  const productBusiness = new ProductBusiness(
+    new ProductDatabaseMock(),
+    new IdGeneratorMock(),
+    new TokenManagerMock()
+  );
+
+  test("deve retornar mensagem e produto criado", async () => {
+    const input = CreateProductSchema.parse({
+      name: "Notebook",
+      price: 20,
+      token: "token-mock-astrodev",
+    });
+
+    const output = await productBusiness.createProduct(input);
+
+    expect(output).toEqual({
+      message: "Producto cadastrado com sucesso",
+      product: {
+        id: "id-mock",
+        name: "Notebook",
+        price: 20,
+        createdAt: expect.any(String),
+      },
+    });
+  });
+
+  test("deve retornar erro de token inválido", async () => {
+    expect.assertions(2);
+    try {
+      const input = CreateProductSchema.parse({
+        name: "Notebook",
+        price: 20,
+        token: "token-invalido",
+      });
+
+      const output = await productBusiness.createProduct(input);
+    } catch (error) {
+      if (error instanceof BadRequestError) {
+        expect(error.statusCode).toBe(400);
+        expect(error.message).toBe("token inválido");
+      }
+    }
+  });
+
+  test("deve retornar erro de acesso", async () => {
+    expect.assertions(2);
+    try {
+      const input = CreateProductSchema.parse({
+        name: "Notebook",
+        price: 20,
+        token: "token-mock-fulano",
+      });
+
+      const output = await productBusiness.createProduct(input);
+    } catch (error) {
+      if (error instanceof BadRequestError) {
+        expect(error.statusCode).toBe(400);
+        expect(error.message).toBe("somente admins podem acessar");
+      }
+    }
+  });
+
+//   test("deve retornar erro de preço inválido pelo Zod", async () => {
+//     expect.assertions(3);
+//     try {
+//       const input = CreateProductSchema.parse({
+//         name: "Notebook",
+//         price: 0,
+//         token: "token-mock-astrodev",
+//       });
+
+//       const output = await productBusiness.createProduct(input);
+//     } catch (error) {
+//       if (error instanceof ZodError) {
+//         console.log(error.issues);
+//         expect(error.issues[0].code).toBe("too_small");
+//         expect(error.issues[0].message).toBe("Number must be greater than 0");
+//         expect(error.issues).toEqual([
+//           {
+//             code: "too_small",
+//             minimum: 0,
+//             type: "number",
+//             inclusive: false,
+//             exact: false,
+//             message: "Number must be greater than 0",
+//             path: ["price"],
+//           },
+//         ]);
+//       }
+//     }
+//   });
+
+  test("deve retornar erro de preço inválido pelo BadRequestError", async () => {
+    expect.assertions(2);
+    try {
+      const input = CreateProductSchema.parse({
+        name: "Notebook",
+        price: 0,
+        token: "token-mock-astrodev",
+      });
+
+      const output = await productBusiness.createProduct(input);
+    } catch (error) {
+      if (error instanceof BadRequestError) {
+        expect(error.statusCode).toBe(400);
+        expect(error.message).toBe("preço não pode ser 0 ou negativo");
+      }
+    }
+  });
+});
--- modulo4-fullstack/aula-testes-backend-III/testes-backend-III-exercicios/tests/business/user/getUsers.test.ts
+++ modulo4-fullstack/aula-testes-backend-III/testes-backend-III-exercicios/tests/business/user/getUsers.test.ts
@@ -1,10 +1,11 @@
-import { UserBusiness } from "../../../src/business/UserBusiness"
-import { GetUsersSchema } from "../../../src/dtos/user/getUsers.dto"
-import { USER_ROLES } from "../../../src/models/User"
-import { HashManagerMock } from "../../mocks/HashManagerMock"
-import { IdGeneratorMock } from "../../mocks/IdGeneratorMock"
-import { TokenManagerMock } from "../../mocks/TokenManagerMock"
-import { UserDatabaseMock } from "../../mocks/UserDatabaseMock"
+import { UserBusiness } from "../../../src/business/UserBusiness";
+import { GetUsersSchema } from "../../../src/dtos/user/getUsers.dto";
+import { BadRequestError } from "../../../src/errors/BadRequestError";
+import { USER_ROLES } from "../../../src/models/User";
+import { HashManagerMock } from "../../mocks/HashManagerMock";
+import { IdGeneratorMock } from "../../mocks/IdGeneratorMock";
+import { TokenManagerMock } from "../../mocks/TokenManagerMock";
+import { UserDatabaseMock } from "../../mocks/UserDatabaseMock";
 
 describe("Testando getUsers", () => {
   const userBusiness = new UserBusiness(
@@ -12,31 +13,63 @@ describe("Testando getUsers", () => {
     new IdGeneratorMock(),
     new TokenManagerMock(),
     new HashManagerMock()
-  )
+  );
 
   test("deve retornar lista de todos users", async () => {
     const input = GetUsersSchema.parse({
-      token: "token-mock-astrodev"
-    })
+      token: "token-mock-astrodev",
+    });
 
-    const output = await userBusiness.getUsers(input)
+    const output = await userBusiness.getUsers(input);
 
-    expect(output).toHaveLength(2)
+    expect(output).toHaveLength(2);
     expect(output).toEqual([
       {
         id: "id-mock-fulano",
         name: "Fulano",
         email: "fulano@email.com",
         createdAt: expect.any(String),
-        role: USER_ROLES.NORMAL
+        role: USER_ROLES.NORMAL,
       },
       {
         id: "id-mock-astrodev",
         name: "Astrodev",
         email: "astrodev@email.com",
         createdAt: expect.any(String),
-        role: USER_ROLES.ADMIN
+        role: USER_ROLES.ADMIN,
       },
-    ])
-  })
-})
+    ]);
+  });
+
+  test("deve retornar erro de token inválido", async () => {
+    expect.assertions(2);
+    try {
+      const input = GetUsersSchema.parse({
+        token: "token-invalido",
+      });
+
+      const output = await userBusiness.getUsers(input);
+    } catch (error) {
+      if (error instanceof BadRequestError) {
+        expect(error.statusCode).toBe(400);
+        expect(error.message).toBe("token inválido");
+      }
+    }
+  });
+
+  test("deve retornar erro de acesso", async () => {
+    expect.assertions(2);
+    try {
+      const input = GetUsersSchema.parse({
+        token: "token-mock-fulano",
+      });
+
+      const output = await userBusiness.getUsers(input);
+    } catch (error) {
+      if (error instanceof BadRequestError) {
+        expect(error.statusCode).toBe(400);
+        expect(error.message).toBe("somente admins podem acessar");
+      }
+    }
+  });
+});
