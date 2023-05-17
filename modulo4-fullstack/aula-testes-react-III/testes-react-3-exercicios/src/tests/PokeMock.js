aa7bfb512642f60f3cbd0448df25ca2e58a8e9db
LATerada <larissaakiterada@gmail.com>
2023-05-17 15:09:24 -0300

    Resolucao dos exercicios da aula-testes-react-III

---
+++ modulo4-fullstack/aula-testes-react-III/testes-react-3-exercicios/src/tests/PokeMock.js
@@ -0,0 +1,10 @@
+export const axiosResponseMock = {
+  data: {
+    name: "Pokemon-Mock",
+    sprites: {
+      front_default: "ImageUrl",
+      id: "1",
+    },
+    types: [{ type: { name: "Type1" } }, { type: { name: "Type2" } }],
+  },
+};
